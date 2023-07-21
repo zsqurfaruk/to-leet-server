f288d072b033e67038583e0d3410168b696ab574
faruk <farukk6319@gmail.com>
2023-07-22 00:07:19 +0600

    22-7-23, share link solve

--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/usersController");
 const verifyToken = require("../middleWare/verifyToken");
-const authorization = require("../middleWare/authorization");
 
 router
   .route("/signup")
